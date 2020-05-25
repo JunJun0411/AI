58e48e1482c64fb7a4f522dbf65a04698f11b94a
JunJun0411 <wnsgudrnrn1@gmail.com>
2020-05-25 18:09:41 +0900

    numerical_gradient 수정

--- gradient.py
+++ gradient.py
@@ -44,10 +44,9 @@ def cross_entropy_error_label(y, t_label):
 # print(cross_entropy_error_label(y1, t1_label))
 # print(cross_entropy_error_label(y2, t2_label))
     
-
-def f1(x):
-    """ 미분 할 식 """
-    return 0.01*x**2 + 0.1*x
+# # 미분 할 식
+# def f1(x):
+#     return 0.01*x**2 + 0.1*x
 
 
 def numerical_difference(f, x):
@@ -62,27 +61,30 @@ def f2(x):
     return np.sum(x**2)
 # print(f2(t2_label))
 
-def numerical_gradient(f, x):
+# original numerical_gradient function
+def numerical_gradient_no_batch_(f, x):
     """ x is a vector containing input value of f at x"""
     h = 1e-4 # 0.0001
     grad = np.zeros_like(x)
 
-#     for i, x in enumerate(x): 
-#         grad[i] = (f(x+h) - f(x-h)) / (2*h)
-    for idx in range(x.size):
-        tmp_val = x[idx]
-        x[idx] =  tmp_val + h
-        fxh1 = f(x)
-        # f(x-h) 계산
-        x[idx] = tmp_val - h
-        fxh2 = f(x)
-        grad[idx] = (fxh1 - fxh2) / (2*h)
-        x[idx] = tmp_val # 값 복원
+    for i, x in enumerate(x): 
+        grad[i] = (f(x+h) - f(x-h)) / (2*h)
 
     return grad
 
-x = np.array([-3.0, 4.0]) # Test Data X
-# print(numerical_gradient(f2, x))
+def numerical_gradient(f, X):
+    if X.ndim == 1:
+        return numerical_gradient_no_batch_(f, X) # original numerical_gradient function
+    else:
+        grad = np.zeros_like(X)
+        for idx, x in enumerate(X):
+            # print("in numerical gradient ", idx, x)
+            grad[idx] = numerical_gradient_no_batch_(f, x)
+        return grad
+
+x = np.array([[-3.0, 4.0],
+             [2.0, 1.0]])
+# print(numerical_gradient(f2, x), x)
 
 def gradient_descent(f, init_x, lr=0.1, epoch=100):
     x = init_x
@@ -93,4 +95,3 @@ def gradient_descent(f, init_x, lr=0.1, epoch=100):
 # print(gradient_descent(f2, x, 0.1, 100))
 # print(gradient_descent(f2, x, 10.0,  100))
 # print(gradient_descent(f2, x, 1e-4, 100000))
-
